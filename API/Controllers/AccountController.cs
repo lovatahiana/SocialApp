1990636a5e574b6404a497f83ae7d5ea7bbc1e2a
lovatahiana <lovatahianar@gmail.com>
2023-06-17 19:28:22 +0300

    Adding Account controlle

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username))
+            {
+                return BadRequest("Username already taken");
+            }
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            await _context.AppUsers.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.AppUsers.AnyAsync(x=>x.UserName == username.ToLower());
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.AppUsers.SingleOrDefaultAsync(x=> x.UserName == loginDto.Username);
+            if(user is null){
+                return Unauthorized("Invalid username");
+            }
+            using var hmac = new HMACSHA512(user.PasswordSalt); 
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Password invalid");
+                }
+            }
+            return user;
+        }
+  
+    }
+}
